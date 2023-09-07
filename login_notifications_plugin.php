dc3ea874d0b2fc5e4d0444babae74b9d616f2a86
Beav <eaver.m@gmail.com>
2023-09-07 12:36:49 -0400

    Add files via upload

--- login_notifications_plugin.php
+++ login_notifications_plugin.php
@@ -60,6 +60,16 @@ class LoginNotificationsPlugin extends Plugin
         // Retrieve email configurations from the plugin's settings
         $emails = Configure::get('LoginNotifications.install.emails');
 
+        // Define initial settings values
+        $LoginNotificationsSettings = [
+            'enable_admin_login_emails' => true,
+            'enable_client_login_emails' => false,
+            'send_on_new_ip' => false,
+        ];
+
+        // Set initial settings values
+        $this->Companies->setSetting(Configure::get('Blesta.company_id'), 'LoginNotificationsPlugin', serialize($LoginNotificationsSettings));
+
         foreach ($emails as $email) {
             // Check if an email group already exists for the specified action
             $group = $this->EmailGroups->getByAction($email['action']);
@@ -115,6 +125,9 @@ class LoginNotificationsPlugin extends Plugin
             Loader::loadModels($this, ['Emails', 'EmailGroups']);
             Configure::load('login_notifications', dirname(__FILE__) . DS . 'config' . DS);
 
+            // Remove plugin settings from the company configuration
+            $this->Companies->unsetSetting(Configure::get('Blesta.company_id'), 'LoginNotificationsPlugin');
+
             // Get the list of emails to be removed from the plugin's configuration
             $emails = Configure::get('LoginNotifications.install.emails');
 
@@ -159,12 +172,13 @@ class LoginNotificationsPlugin extends Plugin
      */
     public function loginHandler(EventInterface $event)
     {
+
         // Get event parameters and return value
         $params = $event->getParams();
         $return = $event->getReturnValue();
 
         // Set the timezone of the $this->Date object to the company's configured timezone in Blesta.
-      //  $this->Date->setTimezone(Configure::get('Blesta.company_timezone'));
+        $this->Date->setTimezone('UTC', Configure::get('Blesta.company_timezone'));
 
         // Get LoginNotifications plugin settings
         $LoginNotificationsSettings = $this->Companies->getSetting($this->company_id, 'LoginNotificationsPlugin');
@@ -178,11 +192,14 @@ class LoginNotificationsPlugin extends Plugin
             // Set the return value for the event and exit the function
             return $event->setReturnValue($return);
         } else {
-            // Check if the IP address matches the one from the 2nd item in the array
-            if ($_SERVER['REMOTE_ADDR'] == $user_logs[1]->ip_address && $settings['send_on_new_ip'] == true) {
-                // Set the return value for the event and exit the function
-                return $event->setReturnValue($return);
-        }
+          // Check if "send_on_new_ip" key exists in the settings array and $_SERVER['REMOTE_ADDR'] is set
+          if (isset($settings['send_on_new_ip']) && $settings['send_on_new_ip'] == true && isset($_SERVER['REMOTE_ADDR'])) {
+              // Check if the IP address matches the one from the 2nd item (previous login) in the array
+              if ($_SERVER['REMOTE_ADDR'] == $user_logs[1]->ip_address) {
+                  // Set the return value for the event and exit the function
+                  return $event->setReturnValue($return);
+              }
+          }
       }
 
         // Check whether GeoIp is enabled
@@ -194,10 +211,12 @@ class LoginNotificationsPlugin extends Plugin
             if (!isset($this->NetGeoIp)) {
                 $this->NetGeoIp = $this->Net->create('NetGeoIp');
             }
-        }
 
-        // Get GeoIp location data for the user's IP address
-        $geoData = $this->NetGeoIp->getLocation($_SERVER['REMOTE_ADDR']);
+            // Get GeoIp location data for the user's IP address
+            $geoData = $this->NetGeoIp->getLocation($_SERVER['REMOTE_ADDR']);
+        } else {
+          $geoData = null;
+        }
 
         // Check if it's a staff login or a client login
         $staff_login = $this->Staff->get($params['user_id']);
@@ -224,33 +243,39 @@ class LoginNotificationsPlugin extends Plugin
      * @param array  $geoData  The GeoIP location data.
      * @param array  $params    The event parameters.
      */
-    public function sendLoginNotificationEmail($type, $user, $settings, $geoData, $params)
-    {
-        // Check if email notifications are enabled for the specific user type
-        if ($settings['enable_' . $type . '_login_emails'] == true) {
-            // Define email content tags
-            $tags = array(
-                'ip_address' => $_SERVER['REMOTE_ADDR'],
-                'user' => $user->username,
-                'date' => date('D, d M Y', strtotime($this->Date->format('c'))),
-                'time' => $this->Date->cast($this->Date->format('c'), 'g:i:s A'),
-                'location' => $geoData['city'] . ', ' . $geoData['region'] . ' - ' . $geoData['country_name'],
-                'recovery_link' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . Configure::get('Blesta.company')->hostname . '/' . Configure::get('Route.' . $type) . '/login/' . ($type == 'client' ? 'forgot' : 'reset'),
-                'first_name' => $user->first_name,
-            );
+     public function sendLoginNotificationEmail($type, $user, $settings, $geoData, $params)
+     {
+         // Check if email notifications are enabled for the specific user type
+         if ($settings['enable_' . $type . '_login_emails'] == true) {
+             // Initialize the tags array
+             $tags = array(
+                 'ip_address' => $_SERVER['REMOTE_ADDR'],
+                 'user' => $user->username,
+                 'date' => $this->Date->cast(time(), 'D, d M Y'),
+                 'time' => $this->Date->cast(time(), 'g:i:s A'),
+                 'recovery_link' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . Configure::get('Blesta.company')->hostname . '/' . Configure::get('Route.' . $type) . '/login/' . ($type == 'client' ? 'forgot' : 'reset'),
+                 'first_name' => $user->first_name,
+             );
 
-            // Send login notification email
-            $this->Emails->send(
-                'LoginNotifications.' . $type . 'login',
-                Configure::get("Blesta.company_id"),
-                NULL,
-                $user->email,
-                $tags,
-                NULL,
-                NULL,
-                NULL,
-                array('to_client_id' => $params['user_id'])
-            );
-        }
-    }
+             // Check if $geoData is not null and contains the expected elements
+             if (!empty($geoData)) {
+                 // Add the "location" key to the tags array
+                 $tags['location'] = $geoData['city'] . ', ' . $geoData['region'] . ' - ' . $geoData['country_name'];
+             }
+
+             // Send login notification email
+             $this->Emails->send(
+                 'LoginNotifications.' . $type . 'login',
+                 Configure::get("Blesta.company_id"),
+                 NULL,
+                 $user->email,
+                 $tags,
+                 NULL,
+                 NULL,
+                 NULL,
+                 array('to_client_id' => $params['user_id'])
+             );
+         }
+     }
+
 }
