3f9a6bfb84b9922003a61a84fb7157c47be13e83
Beav <eaver.m@gmail.com>
2023-09-07 16:23:02 -0400

    Add files via upload

--- controllers/admin_manage_plugin.php
+++ controllers/admin_manage_plugin.php
@@ -14,9 +14,6 @@ class AdminManagePlugin extends AppController
         Loader::loadModels($this, ['Companies']);
         $this->company_id = Configure::get('Blesta.company_id');
 
-        // Get settings related to the LoginNotificationsPlugin
-        $this->LoginNotificationsSettings = $this->Companies->getSetting($this->company_id, 'LoginNotificationsPlugin');
-
         // Require login to access this page
         $this->parent->requireLogin();
 
@@ -31,41 +28,41 @@ class AdminManagePlugin extends AppController
     /**
      * Index action for displaying and handling Login Notifications Plugin settings.
      */
-    public function index()
-    {
-        // Initialize the controller
-        $this->init();
-        $this->uses(['Settings', 'Plugins']);
+     public function index()
+     {
+         // Initialize the controller
+         $this->init();
+         $this->uses(['Settings', 'Plugins']);
 
-        // Get the plugin ID from the URL or set it to null
-        $plugin_id = (isset($this->get[0]) ? $this->get[0] : null);
+         // Get the plugin ID from the URL or set it to null
+         $pluginId = $this->get[0] ?? null;
 
-        // Create a new instance of the Login Notifications Plugin
-        $plugin = $this->Plugins->create('login_notifications');
+         // Create a new instance of the Login Notifications Plugin
+         $plugin = $this->Plugins->create('login_notifications');
 
-        // Initialize settings as an empty object or load existing settings
-        $settings = (object) ($this->LoginNotificationsSettings
-            ? unserialize($this->LoginNotificationsSettings->value)
-            : []);
+         // Initialize settings as an empty object or load existing settings
+         $settings = $this->LoginNotificationsSettings
+             ? unserialize($this->LoginNotificationsSettings->value)
+             : (object)[];
 
-        // Handle form submissions (redirect if the form is submitted)
-        if (!empty($this->post)) {
-            // Save the updated settings to the database
-            $this->Companies->setSetting($this->company_id, 'LoginNotificationsPlugin', serialize($this->post));
-            $this->setMessage('success', "Settings Saved Successfully!", false, null, false);
-            $settings = (object)$this->post;
-            $this->set('plugin_id', $plugin_id);
-            $this->set('settings', $settings);
-            $this->parent->flashmessage('message', 'Success: Settings Saved Successfully!');
+         // Handle form submissions (redirect if the form is submitted)
+         if (!empty($this->post)) {
+             // Save the updated settings to the database
+             $this->Companies->setSetting($this->company_id, 'LoginNotificationsPlugin', serialize($this->post));
+             $settings = (object)$this->post;
 
-            // Render the admin_manage_plugin partial with updated data
-            $this->partial('admin_manage_plugin', compact("settings", "plugin_id"));
+             // Flash a success message
+             $this->parent->flashmessage('message', Language::_('LoginNotificationsPlugin.!success.save', true));
 
-            // Redirect to the appropriate plugin management page (replace $plugin_id with the actual plugin ID)
-            $this->redirect($this->base_uri . 'settings/company/plugins/manage/' . $plugin_id . '/');
-        }
+             // Redirect to the appropriate plugin management page
+             $this->redirect($this->base_uri . 'settings/company/plugins/manage/' . $pluginId . '/');
+         }
 
-        // Return the view with the settings data
-        return $this->partial('admin_manage_plugin', compact("settings"));
-    }
+         // Set variables for the view
+         $this->set('plugin_id', $pluginId);
+         $this->set('settings', $settings);
+
+         // Render the admin_manage_plugin partial with updated data
+         return $this->partial('admin_manage_plugin', compact("settings", "pluginId"));
+     }
 }
--- language/en_us/login_notifications_plugin.php
+++ language/en_us/login_notifications_plugin.php
@@ -2,6 +2,19 @@
 /**
  * en_us language for the Login Notifications plugin.
  */
+
 // Basics
-$lang['LoginNotificationsPlugin.name'] = 'Login Notifications';
-$lang['LoginNotificationsPlugin.description'] = 'Adds an option to email staff and clients upon their login';
+$lang['LoginNotificationsPlugin.name'] = 'Login Notifications'; // Plugin name
+$lang['LoginNotificationsPlugin.description'] = 'Adds an option to email staff and clients upon their login'; // Plugin description
+
+// Configuration options
+$lang['LoginNotificationsPlugin.enable_admin_login_emails'] = "Enable Admin Login Emails"; // Option to enable admin login emails
+$lang['LoginNotificationsPlugin.enable_client_login_emails'] = "Enable Client Login Emails"; // Option to enable client login emails
+
+$lang['LoginNotificationsPlugin.send_on_new_ip'] = "Send Email ONLY on IP change"; // Option to send email only on IP change
+
+$lang['LoginNotificationsPlugin.title.general'] = "General Settings"; // Title for general settings
+$lang['LoginNotificationsPlugin.title.additional_settings'] = "Additional Settings"; // Title for additional settings
+
+$lang['LoginNotificationsPlugin.!success.save'] = "Success: Settings Saved Successfully!"; // Message for successful settings submission
+?>
